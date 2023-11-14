6dca8f5374c0e8d0c0cb9daab93f59399fe5fefa
hyunzsu <kkm5169@naver.com>
2023-11-14 16:13:16 +0900

    [💄 style] brand color 변경

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FiShoppingBag } from 'react-icons/fi';
+import { GrYoga } from 'react-icons/gr';
 import { BsFillPencilFill } from 'react-icons/bs';
 import { login, logout, onUserStateChange } from '../api/firebase';
 import User from './User';
@@ -16,8 +16,8 @@ export default function Navbar() {
     <div>
       <header className='flex justify-between border-b border-gray-300 p-2'>
         <Link to='/' className='flex items-center text-4xl text-brand'>
-          <FiShoppingBag />
-          <h1>hyunzsu</h1>
+          <GrYoga />
+          <h1>Yoga</h1>
         </Link>
         <nav className='flex items-center gap-4 font-semibold'>
           <Link to='/products'>Products</Link>
