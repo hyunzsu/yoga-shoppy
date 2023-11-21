85b4e39db7d2f5f747f9a909e29635a3e9c01182
hyunzsu <kkm5169@naver.com>
2023-11-21 23:27:05 +0900

    [💄 style] 어드민사용자 UI 완성 - 어드민 계정일 때만 쓰기 아이콘 보이는 기능 완성 - 로그인 하지 않은 사용자가 도메인에서 /products/new 경로를 입력할 때 접근 가능한 이슈 수정 못함

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { GrYoga } from 'react-icons/gr';
 import { BsFillPencilFill } from 'react-icons/bs';
 import { login, logout, onUserStateChange } from '../api/firebase';
 import User from './User';
+import Button from './ui/Button';
 
 export default function Navbar() {
   const [user, setUser] = useState();
@@ -20,17 +21,19 @@ export default function Navbar() {
       <header className='flex justify-between border-b border-gray-300 p-2'>
         <Link to='/' className='flex items-center text-4xl text-brand'>
           <GrYoga />
-          <h1>Yoga</h1>
+          <h1 className='font-semibold'>Yoga</h1>
         </Link>
         <nav className='flex items-center gap-4 font-semibold'>
           <Link to='/products'>Products</Link>
           <Link to='/carts'>Carts</Link>
-          <Link to='/products/new' className='text-2xl'>
-            <BsFillPencilFill />
-          </Link>
+          {user && user.isAdmin && (
+            <Link to='/products/new' className='text-2xl'>
+              <BsFillPencilFill />
+            </Link>
+          )}
           {user && <User user={user} />}
-          {!user && <button onClick={login}>Login</button>}
-          {user && <button onClick={logout}>Logout</button>}
+          {!user && <Button text={'Login'} onClick={login} />}
+          {user && <Button text={'Logout'} onClick={logout} />}
         </nav>
       </header>
     </div>
