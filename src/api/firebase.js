6886a8233b112837ad1bff857b9d090ded449df9
hyunzsu <hyunzsu@naver.com>
2024-01-09 16:34:17 +0900

    [✨ feat] 파이어베이스에 장바구니 제품 읽고, 추가, 삭제하는 함수 제작

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -7,7 +7,7 @@ import {
   signOut,
   onAuthStateChanged,
 } from 'firebase/auth';
-import { getDatabase, ref, get, set } from 'firebase/database';
+import { getDatabase, ref, get, set, remove } from 'firebase/database';
 
 /* Firebase 초기화 */
 const firebaseConfig = {
@@ -32,7 +32,7 @@ export function logout() {
   signOut(auth).catch(console.error);
 }
 
-/* 로그인 세션 정보 유지 */
+/* 로그인 세션 정보 유지 - 유저 상태 변경이 궁금할 때 */
 export function onUserStateChange(callback) {
   onAuthStateChanged(auth, async (user) => {
     // 1. 사용자가 있는 경우에 (로그인 한 경우)
@@ -43,7 +43,7 @@ export function onUserStateChange(callback) {
   });
 }
 
-/* isAdmin을 넣어서 객체로 리턴 */
+/* 어드민 유저인지 확인 */
 async function adminUser(user) {
   // 2. 사용자가 어드민 권한을 가지고 있는지 확인!
   // 3. {...user, isAdmin: true/false}
@@ -63,7 +63,6 @@ async function adminUser(user) {
 export async function addNewProduct(product, imageUrl) {
   // 고유한 ID 생성
   const id = uuid();
-
   // 파이어베이스의 'products' 경로에 새로운 제품 정보를 등록
   return set(ref(database, `products/${uuid()}`), {
     ...product, // 기존의 받아온 product에 있는 모든 key와 value를 복사해옴
@@ -83,4 +82,23 @@ export async function getProducts() {
       }
       return [];
     });
+}
+
+/* 쇼핑카트 읽어오기 */
+export async function getCart(userId) {
+  return get(ref(database, `carts/${userId}`)) //
+    .then((snapshot) => {
+      const items = snapshot.val() || {};
+      return Object.values(items); // 값만 읽어옴
+    });
+}
+
+/* 카트에 제품 추가 */
+export async function addOrUpdateToCart(userId, product) {
+  return set(ref(database, `carts/${userId}/${product.id}`), product); // carts안에 사용자 id 안에 제품정보 추가
+}
+
+/* 카트에 제품 삭제 */
+export function removeFromCart(userId, productId) {
+  return remove(ref(database, `carts/${userId}/${productId}`));
 }
